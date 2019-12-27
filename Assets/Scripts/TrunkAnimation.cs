ce024c9733fae0d257544af5370c5533d4f4b578
Will <will.lucen4@gmail.com>
2019-12-26 23:27:58 -0300

    Random tree instantiated with different animation curves, working on particle and collapse

---
+++ Assets/Scripts/TrunkAnimation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunkAnimation : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve linearCurve;
+    [SerializeField] private AnimationCurve popupCurve;
+    [SerializeField] private AnimationCurve bounceCurve;
+    
+    [Header ("Target size")]
+    [SerializeField] private Vector3 finalPosition;
+    [Header ("Animation duration")]
+    [SerializeField] private float time;
+    
+    private Vector3 initialPosition;
+    
+    private IEnumerator TranslateObject (AnimationCurve curve)
+    {
+        float i = 0;
+        float rate = 1 / time;
+        while (i < 1) {
+            i += rate * Time.deltaTime;
+            transform.localPosition = Vector3.Lerp (initialPosition, finalPosition, curve.Evaluate (i));
+            yield return null;
+        }
+    }
+
+    public void startAnimation()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TranslateObject(linearCurve));
+    }
+
+    public void bounceAnimation()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TranslateObject(bounceCurve));
+    }
+
+    public void popupAnimation()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TranslateObject(popupCurve));
+    }
+    
+    public void updateInitialPosition(Vector3 pos)
+    {
+        initialPosition = pos;
+    }
+}
